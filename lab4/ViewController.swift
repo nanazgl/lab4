0b7b5514f790463ed1101b5b056205edbbc3ba7f
nanazgl <31530@iitu.edu.kz>
2024-02-28 22:56:01 +0600

    Final

--- lab4/ViewController.swift
+++ lab4/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var setTextButton: UIButton!
+    @IBOutlet weak var clearTextButtom: UIButton!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clearTextButton(_ sender: Any) {
+        label.text=""
+        label.textColor = UIColor.red
+    }
+    @IBAction func setTextButton(_ sender: Any) {
+        label.text = textField.text
+    }
+    
+    
 }
 
